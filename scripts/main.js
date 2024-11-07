c0676aac6a8ed63042df264a9d03878ab1ef6b0d
jess-cattanach <jess28catt@gmail.com>
2024-11-07 13:27:35 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+let growth = document.querySelector('#growth');
+growth.onclick = () => {
+    if (growth.getAttribute('src') === 'images/littlebabies.jpg')
+    {
+        growth.setAttribute('src', 'images/grownbabies.jpg');
+    }
+    else{
+        growth.setAttribute('src', 'images/littlebabies.jpg');
+    }
+};
+
+let deviousCricket = document.querySelector('#cricketTrouble');
+deviousCricket.onmouseover = () => {
+    if (deviousCricket.getAttribute('src') === 'images/cricketgoldfish.jpg')
+        {
+            deviousCricket.setAttribute('src', 'images/cricketinsink.jpg');
+        }
+        else{
+            deviousCricket.setAttribute('src', 'images/cricketgoldfish.jpg');
+        }
+};
+
+let evilTex = document.querySelector('#texasTrouble');
+evilTex.onmouseover = () => {
+    if (evilTex.getAttribute('src') === 'images/texasundercouch.jpg')
+        {
+            evilTex.setAttribute('src', 'images/inbox.jpg');
+        }
+        else{
+            evilTex.setAttribute('src', 'images/texasundercouch.jpg');
+        }
+};
